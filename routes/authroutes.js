c157348600fea5c343fbefb41affa033746b52b9
pranav gupta <pg07codes@gmail.com>
2018-05-23 08:43:50 +0530

    home nav

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -9,6 +9,10 @@ router.post("/signup",(r,s)=>{
     insertuser(r,s)
 })
 
+router.get("/signin",(r,s)=>{
+
+})
+
 router.post("/signin",passport.authenticate('local', {
     successRedirect: `/auth/profile`,
     failureRedirect: '/?ein=true'
